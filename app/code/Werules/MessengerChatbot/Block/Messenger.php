2fb56156e8a5a3bc8b4f4871bed9dbee68f89cc8
blopa <montenegro.pablo@outlook.com>
2017-12-26 20:51:12 -0200

    create initial files

---
+++ app/code/Werules/MessengerChatbot/Block/Messenger.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Magento Messenger Chatbox
+ * Copyright (C) 2017 Pablo Montenegro
+ *
+ * This file is part of Werules/MessengerChatbox.
+ *
+ * Werules/MessengerChatbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Werules\MessengerChatbox\Block;
+
+class Messenger extends \Magento\Framework\View\Element\Template
+{
+    protected $_helper;
+    protected $_chatbotAPI;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Werules\Chatbot\Helper\Data $helperData,
+        \Werules\Chatbot\Model\ChatbotAPI $chatbotAPI,
+        array $data = array()
+    )
+    {
+        $this->_chatbotAPI = $chatbotAPI;
+        $this->_helper = $helperData;
+        parent::__construct($context, $data);
+    }
+
+    private function getMessengerInstance()
+    {
+        $api_token = $this->_helper->getConfigValue('werules_chatbot_messenger/general/api_key');
+        $messenger = $this->_chatbotAPI->initMessengerAPI($api_token);
+        return $messenger;
+    }
+
+    public function getFacebookPageId()
+    {
+        $pageId = $this->getConfigValue('werules_chatbot_messenger/general/page_id');
+        if ($pageId)
+            return $pageId;
+
+        $messengerInstance = $this->getMessengerInstance();
+        $pageDetails = $messengerInstance->getPageDetails();
+        if (isset($pageDetails['id']))
+        {
+            $pageId = $pageDetails['id'];
+            $this->setConfigValue('werules_chatbot_messenger/general/page_id', $pageId);
+            return $pageId;
+        }
+
+        return '';
+    }
+
+    public function getFacebookAppId()
+    {
+        $appId = $this->getConfigValue('werules_chatbot_messenger/general/app_id');
+        return $appId;
+    }
+
+    public function isDomainWhitelabeled()
+    {
+        $enable = $this->getConfigValue('werules_chatbot_messenger/general/domain_whitelisted');
+        if ($enable)
+            return true;
+
+        $messengerInstance = $this->getMessengerInstance();
+//        $url = parse_url($_SERVER['SERVER_NAME'], PHP_URL_HOST);
+        $url = $_SERVER['SERVER_NAME'];
+        $domain = array($url);
+        $result = $messengerInstance->addDomainsToWhitelist($domain);
+        if (!isset($result['error']))
+        {
+            $this->setConfigValue('werules_chatbot_messenger/general/domain_whitelisted', $this->_define::WHITELABELED);
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isChatboxEnabled()
+    {
+        $enable = $this->getConfigValue('werules_chatbot_messenger/general/enable_messenger_box');
+        $isWhitelabeled = $this->isDomainWhitelabeled();
+        if ($enable && $isWhitelabeled)
+            return true;
+
+        return false;
+    }
+
+    private function getConfigValue($code)
+    {
+        return $this->_helper->getConfigValue($code);
+    }
+
+    private function setConfigValue($field, $value)
+    {
+        $this->_helper->setConfigValue($field, $value);
+    }
+}
---
+++ app/code/Werules/MessengerChatbot/Helper/Data.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Magento Chatbot Integration
+ * Copyright (C) 2017
+ *
+ * This file is part of Werules/Chatbot.
+ *
+ * Werules/Chatbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Werules\MessengerChatbox\Helper;
+
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    protected $_configWriter;
+
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Framework\App\Config\Storage\WriterInterface $configWriter
+    )
+    {
+        $this->_configWriter = $configWriter;
+        parent::__construct($context);
+    }
+
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field, ScopeInterface::SCOPE_STORE, $storeId
+        );
+    }
+
+    public function setConfigValue($field, $value)
+    {
+        $this->_configWriter->save($field, $value);
+    }
+}
